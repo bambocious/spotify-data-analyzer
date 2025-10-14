20a801ca1759bf29cc5a7025857905413e696c8a
Nathan <nwwoolsey@fastmail.com>
2025-10-14 09:14:45 -0700

    Implement file choice, remove an old JS file

--- main.py
+++ main.py
@@ -5,10 +5,17 @@ if __name__ == "__main__":
     dataDir = input("Absolute location of Spotify history files: ")
     
     histFiles = []
-
     for dirpath, _, filenames in os.walk(dataDir):
         for name in filenames:
             if os.path.splitext(name)[1].lower() == ".json":
                 histFiles.append(os.path.join(dirpath,name))
     
+    print(histFiles)
 
+    data = {}
+    for file in histFiles:
+        print("file: " + file)
+        with open(file) as json_data:
+            data.update(json.load(json_data))
+
+    print(data)
