b9738a8a86731945ffdca56b0886cecf0e7a274c
Nathan <nwwoolsey@fastmail.com>
2025-10-15 16:13:59 -0700

    Black formatting, expand filterByKey

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
-import json # Spotify history is exported in JSON
-import os # for working with files provided by the user
+import json  # Spotify history is exported in JSON
+import os  # for working with files provided by the user
 
-import processing # Local file with the important functions
+import processing  # Local file with the important functions
 
 if __name__ == "__main__":
-    # Currently the debug toggle is hardcoded. 
+    # Currently the debug toggle is hardcoded.
     # I will likely keep it that way, or make it a flag that can be set when executing this application through a terminal.
     debug = True
 
     # TODO: Reimplement this differently when we move to a GUI application. As a script this works fine.
-    dataDir = input("Please enter the absolute location of Spotify history files (\".\" is also accepted): ")
-    
+    dataDir = input(
+        'Please enter the absolute location of Spotify history files ("." is also accepted): '
+    )
+
     # Collect the files containing Spotify history within the directory provided by the user.
     # Currently pulls all JSON files with the correct prefix in their filename into the following array.
     histFiles = []
@@ -18,28 +20,35 @@ if __name__ == "__main__":
         for name in filenames:
             # Since we will be checking the filename and extension more than once, let's make it a variable.
             nameAndExt = os.path.splitext(name)
-            
+
             # If the file does not match the filename and extension, then move on.
-            if nameAndExt[0].find("Streaming_History_Audio") == -1: break
-            if nameAndExt[1].lower() != ".json": break
+            if nameAndExt[0].find("Streaming_History_Audio") == -1:
+                break
+            if nameAndExt[1].lower() != ".json":
+                break
 
             # Otherwise, add it to the list.
-            histFiles.append(os.path.join(dirpath,name))
-                
+            histFiles.append(os.path.join(dirpath, name))
+
     # Check what files were collected to see if the filter worked or if the right files were passed in.
-    if debug: print(histFiles) 
+    if debug:
+        print(histFiles)
 
     # Now, we extract the JSON files into one big list of dictionaries.
     data = []
     for file in histFiles:
         with open(file) as json_data:
             data.extend(json.load(json_data))
-    
-    # Now that the list is complete, we can process it in various ways. 
-    # Eventually the GUI application will show either a bunch of things or you can choose what it will show. 
-    # For now, I could just ask the user for how they want their data processed. 
+
+    # Now that the list is complete, we can process it in various ways.
+    # Eventually the GUI application will show either a bunch of things or you can choose what it will show.
+    # For now, I could just ask the user for how they want their data processed.
     # Either way, I need to write the data processing functions in Python before I can continue further,
     # which will exist in a separate file.
 
     # This is a line used for testing functions from processing.py.
-    print(processing.filterByTitle(data, "Catch the Rainbow"))
+    print(
+        processing.filterByKey(
+            data, "master_metadata_track_name", "Catch the Rainbow", False, False
+        )
+    )
--- processing.py
+++ processing.py
@@ -2,34 +2,32 @@
 First I need to convert all the old JS functions into Python.
 Only after that is done can I consider adding more.
 """
-import re # To filter against regex
+
+import re  # To filter against regex
 
 
-# Return a list only containing dictionaries that match the key-value pair. This is sort of a master-function that all the other filters can use.
-def filterByKey(dataset: list, key, value):
-    filteredSet = [
-        s for s in dataset if s[key] == value
-    ]
+# Return a list only containing dictionaries that match the key-value pair (case sensitivity and exactness can be chosen). This is sort of a master-function that all the other filters can use.
+# TODO: Something needs to process out NoneTypes, as they are affecting more than just regex. They either need to be ignored or need to be turned into empty strings.
+def filterByKey(dataset: list, key, value, isExact, isCaseSensitive):
+    if isCaseSensitive:
+        if isExact:
+            # Case-sensitive and exact:
+            filteredSet = [s for s in dataset if s[key] == value]
+            return filteredSet
+        # Otherwise (case-sensitive but not exact):
+        filteredSet = [s for s in dataset if s[key].find(value) >= 0]
+        return filteredSet
+    if isExact:
+        # Case-insensitive and exact:
+        filteredSet = [s for s in dataset if s[key].lower() == value.lower()]
+    # Case-insensitive and not exact:
+    filteredSet = [s for s in dataset if s[key].lower().find(value.lower()) >= 0]
 
-    return filteredSet
-
-# These next few functions will be mostly implementations of filterByKey. Technically, they are all unnecessary, however I believe it will simplify things in the future.
-
-# ... matching the provided song title. 
-def filterByTitle(dataset: list, title):
-    filteredSet = [
-        # Case insensitive matching + can just contain the song title.
-        s for s in dataset if s["master_metadata_track_name"].lower().find(title.lower())
-    ]
-
-    return filteredSet
 
 # ... matching provided key-regex pair.
 def filterByRegex(dataset: list, key, regex):
     pattern = re.compile(regex)
-    filteredSet = [
-        s for s in dataset if s[key] != None and pattern.search(s[key])
-    ]
-    
-    return filteredSet
+    # I have to check for None types because the regex-matching will through an error if it tries to match for one.
+    filteredSet = [s for s in dataset if s[key] != None and pattern.search(s[key])]
 
+    return filteredSet
